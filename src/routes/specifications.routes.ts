99fd05584ee8b07c0e22a41542a55d89fe80d341
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2022-11-20 22:57:52 -0300

    feat: add bearer autentication

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from "express";
 
+import { ensureAuthenticate } from "../middlewares/ensureAuthenticate";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationRoutes.use(ensureAuthenticate);
 specificationRoutes.post("/", createSpecificationController.handle);
 
 export { specificationRoutes };
