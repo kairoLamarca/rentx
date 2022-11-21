a2cf46b763900a3b2b0c5c008bb6360ecf187f88
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2022-11-20 22:01:49 -0300

    feat: add jwt authentication

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private readonly usersRepository: IUsersRepository
+    ) {}
+
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new Error("Email ou password incorrect!");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error("Email ou password incorrect!");
+        }
+
+        // chave MD5
+        const token = sign({}, "22f55bbf6ddc81e7c41829b528a36f6b", {
+            subject: user.id,
+            expiresIn: "1d",
+        });
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email,
+            },
+        };
+
+        return tokenReturn;
+    }
+}
+
+export { AuthenticateUserUseCase };
---
+++ src/routes/authenticate.routes.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Router } from "express";
+
+import { AurhenticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+
+const authenticateRoutes = Router();
+
+const authenticateUserController = new AurhenticateUserController();
+
+authenticateRoutes.post("/sessions", authenticateUserController.handle);
+
+export { authenticateRoutes };
