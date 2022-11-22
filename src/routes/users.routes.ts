7b40aff850eff328f43c43e99af7c6dc76e63e77
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2022-11-21 23:30:03 -0300

    feat: upload avatar file

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,12 +1,26 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticate } from "../middlewares/ensureAuthenticate";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const userRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 userRoutes.post("/", createUserController.handle);
+
+userRoutes.patch(
+    "/avatar",
+    ensureAuthenticate,
+    uploadAvatar.single("avatar"),
+    updateUserAvatarController.handle
+);
 
 export { userRoutes };
