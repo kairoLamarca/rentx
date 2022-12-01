d0268443680314b4f16e915b80e0e1ef7b5bcbc0
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2022-11-30 21:17:20 -0300

    feat: add create car and user admin

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { hash } from "bcrypt";
+import { getConnection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import createConnection from "../index";
+
+async function create() {
+    const connection = await createConnection("localhost");
+
+    const id = uuidV4();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+        `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+            values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXX')`
+    );
+
+    await connection.close;
+}
+
+create().then(() => console.log("User admin created!"));
