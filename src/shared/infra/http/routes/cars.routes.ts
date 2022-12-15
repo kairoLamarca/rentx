494003e62d38a6fdeda9569190cddff2786561d3
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2022-12-14 21:36:34 -0300

    feat: add car_specifications

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,6 +2,7 @@
 import { Router } from "express";
 
 import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController";
+import { CreateCarSpecificationController } from "../../../../modules/cars/useCases/createCarSpecification/CreateCarSpecificationsController";
 import { ListAvailableCarsController } from "../../../../modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticate } from "../middlewares/ensureAuthenticate";
@@ -10,6 +11,7 @@ const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.post(
     "/",
@@ -19,5 +21,12 @@ carsRoutes.post(
 );
 
 carsRoutes.get("/available", listAvailableCarsController.handle);
+
+carsRoutes.post(
+    "/specifications/:id",
+    ensureAuthenticate,
+    ensureAdmin,
+    createCarSpecificationController.handle
+);
 
 export { carsRoutes };
